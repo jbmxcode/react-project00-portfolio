539bc4e24bb8b8184c37382d8f67ae0f5ba1a7cf
jbmxworld@gmail.com <jbmxworld@gmail.com>
2020-10-20 14:05:02 -0500

    email address link

--- src/components/pages/Contact/Contact.js
+++ src/components/pages/Contact/Contact.js
@@ -12,8 +12,9 @@ function Contact() {
             <div className="container">
                 <section className="contact">
                     <nav className="action-links-group">
-                        <a href="pdf/jbmresume-2020.pdf" className="action-link" target="_blank">Hoja de vida</a>
-                        <a href="https://wa.me/573058537806" className="action-link">+57 3058537806</a>
+                        <a href="pdf/jbmresume-2020.pdf" className="action-link" target="_blank" rel="noopener noreferrer">Hoja de vida</a>
+                        <a href="https://wa.me/573058537806" className="action-link" target="_blank" rel="noopener noreferrer">+57 3058537806</a>
+                        <a href="mailto:info@josuebaloy.com" className="action-link" target="_blank" rel="noopener noreferrer">info@josuebaloy.com</a>
                     </nav>
                 </section>
             </div>
